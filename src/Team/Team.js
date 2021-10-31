f79f909efc80e95069644486ff1d1917448563ad
AleksandraLiadova <56653235+AleksandraLiadova@users.noreply.github.com>
2021-10-31 18:11:24 +0300

    finished what-is-ergonza-desctop

--- src/Team/Team.js
+++ src/Team/Team.js
@@ -57,8 +57,6 @@ export const Team = () => {
     });
   };
 
-  console.log(selected);
-
   return (
     <>
       <Header />
--- src/WhatIsErgonza/WhatIsErgonza.js
+++ src/WhatIsErgonza/WhatIsErgonza.js
@@ -21,6 +21,43 @@ export const WhatIsErgonza = () => {
 
   const demo = useSelector((state) => state.demo);
 
+  const [selected, setSelected] = useState({
+    1: true,
+    2: true,
+    3: true,
+    4: true,
+  });
+
+  const handleMouseEvent = (e) => {
+    let id;
+    console.log(e.target)
+    if( e.target.className === 'layout-img') {
+      id = e.target.id; 
+    } else {
+      id = e.target.parentNode.id; 
+      console.log(e.target.parentNode.id, 'else')
+    }
+    setSelected({
+      1: false,
+      2: false,
+      3: false,
+      4: false,
+      ...{
+        [id]: true,
+      },
+    });
+  };
+
+  const onLeave = (e) => {
+    const id = e.target.id;
+    setSelected({
+      1: true,
+      2: true,
+      3: true,
+      4: true,
+    });
+  }
+
   return (
     <>
       <Header />
@@ -61,18 +98,18 @@ export const WhatIsErgonza = () => {
               {t("what-is-ergonza-text")}
             </div>
           </div>
-          <div className="case-study-page-block-4 what-is-ergonza-images">
-            <div className="images-approach-wrapper">
-              <div className="approach-img-1">
-                <div className="layout-img">
+          <div className="case-study-page-block-4 what-is-ergonza-images" >
+            <div className="images-approach-wrapper" >
+              <div className="approach-img-1 border" style={{opacity: selected[1] ? '1' : '0.2'}} onMouseEnter={handleMouseEvent} onMouseLeave={onLeave}>
+                <div className="layout-img" id={1}>
                   <div className="approach-text-item-1 ergonza-text">
                     <span className="span-header">{t("what-is-ergonza-header-1")}</span>
                     {t("what-is-ergonza-text-1")}
                   </div>
                 </div>
               </div>
-              <div className="approach-img-2">
-                <div className="layout-img">
+              <div className="approach-img-2 border" style={{opacity: selected[2] ? '1' : '0.2'}} onMouseEnter={handleMouseEvent} onMouseLeave={onLeave}>
+                <div className="layout-img" id={2}>
                   <div className="approach-text-item-2 ergonza-text">
                   <span className="span-header">{t("what-is-ergonza-header-2")}</span>
                     {t("what-is-ergonza-text-2")}
@@ -83,16 +120,16 @@ export const WhatIsErgonza = () => {
             
             <div className="ergonza-logo-circle"><div className="ergonza-logo"></div></div>
             <div className="images-approach-wrapper">
-              <div className="approach-img-3">
-                <div className="layout-img">
+              <div className="approach-img-3 border" style={{opacity: selected[3] ? '1' : '0.2'}} onMouseEnter={handleMouseEvent} onMouseLeave={onLeave}>
+                <div className="layout-img" id={3}>
                   <div className="approach-text-item-3 ergonza-text">
                   <span className="span-header">{t("what-is-ergonza-header-3")}</span>
                     {t("what-is-ergonza-text-3")}
                   </div>
                 </div>
               </div>
-              <div className="approach-img-4">
-                <div className="layout-img">
+              <div className="approach-img-4 border" style={{opacity: selected[4] ? '1' : '0.2'}} onMouseEnter={handleMouseEvent} onMouseLeave={onLeave}>
+                <div className="layout-img" id={4}>
                   <div className="approach-text-item-4 ergonza-text">
                   <span className="span-header">{t("what-is-ergonza-header-4")}</span>
                     {t("what-is-ergonza-text-4")}
